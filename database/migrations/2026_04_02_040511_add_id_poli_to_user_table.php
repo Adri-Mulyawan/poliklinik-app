dbfd5e2b3d209f37249328495aa18c7449e58bcc
Adri-Mulyawan <crujer78@gmail.com>
2026-04-02 15:03:40 +0700

    Tugas:1

--- database/migrations/2026_04_02_040511_add_id_poli_to_user_table.php
+++ database/migrations/2026_04_02_040511_add_id_poli_to_user_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
                 $table->foreignId('id_poli')->nullable()->constrained('poli')->cascadeOnDelete();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
                 $table->dropForeign(['id_poli']);
                 $table->dropColumn('id_poli');
         });
